95490744f93e26069a29ff8734aac63604649554
aviisleep <aviisleep@gmail.com>
2022-04-21 15:11:19 -0500

    algoritmo 2 terminado

--- 07-Algoritmos-II/homework/homework.js
+++ 07-Algoritmos-II/homework/homework.js
@@ -6,6 +6,26 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  var len = array.length;
+
+  if(len <= 1) {
+  	return array;
+  }
+
+  var pivot = array[0];
+
+  var left = [];
+  var right = [];
+
+  for(var i = 1; i < len; i++) {
+  	if(array[i] < pivot) {
+  		left.push(array[i]);
+  	} else {
+  		right.push(array[i]);
+  	}
+  }
+
+  return quickSort(left).concat(pivot, quickSort(right));
 
 }
 
@@ -14,7 +34,29 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  var len = array.length;
 
+  if( len <= 1) {
+  	return array;
+  }
+
+  var middle = Math.floor(len/2);
+  var left = array.slice(0, middle);
+  var right = array.slice(middle);
+
+  right = mergeSort(right);
+  left = mergeSort(left);
+
+  	var arr = [];
+
+	while (left.length && right.length) {
+		if(left[0] < right[0]) {
+			arr.push(left.shift());
+		} else {
+			arr.push(right.shift());
+		}
+	}
+	return arr.concat(left, right);
 }
 
 // No modificar nada debajo de esta línea
